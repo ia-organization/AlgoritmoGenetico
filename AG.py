8fafe4dac0624ebc7f30d5335140401360bc6066
Jonathan Ribeiro da Silva <jota.ribeiro.silva@gmail.com>
2019-05-22 19:39:20 -0300

    AG

---
+++ AG.py
@@ -0,0 +1,21 @@
+class Individual(object):
+    def __init__(self, parameter_list):
+        pass
+
+class Population(object):
+    individuals = list()
+
+    def __init__(self, parameter_list):
+        pass
+    
+    def swing(self, parameter_list):
+        pass
+    
+    def chernobyl(self):
+        pass
+    
+def AlgotimGenetic():
+    pass
+
+if __name__ == "__main__":
+    AlgotimGenetic()
